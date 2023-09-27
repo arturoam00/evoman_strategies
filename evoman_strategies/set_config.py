304975cad1dcb335cbf4f2819e905c9631c55a3a
Arturo Abril Martinez <arturoabrilm@gmail.com>
2023-09-27 19:53:05 +0200

    make sure the enemies parameter is valid

--- evoman_strategies/set_config.py
+++ evoman_strategies/set_config.py
@@ -2,10 +2,15 @@ import json
 import sys
 
 
-def main(id_1="EA1", id_2="EA2", enemies=2, pop_size=100, n_gens=30, upper=1, lower=-1):
+def main(
+    id_1="EA1", id_2="EA2", enemies="2", pop_size=100, n_gens=30, upper=1, lower=-1
+):
+    if enemies not in "12345678":
+        raise ValueError(f"Invalid enemies {enemies}")
+
     cfg = {
-        "id_1": id_1,
-        "id_2": id_2,
+        "id_1": str(id_1),
+        "id_2": str(id_2),
         "enemies": str(enemies),
         "pop_size": int(pop_size),
         "n_gens": int(n_gens),
