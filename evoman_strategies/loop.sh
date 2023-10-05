5002e7aa487455fe0c36aa7c729c2a18707016b5
Arturo Abril Martinez <arturoabrilm@gmail.com>
2023-10-05 23:05:36 +0200

    Added folders for EAs storing (generalists; specialists) and file combine_boxplots.py to present indi_gain of multiple enemies at the same time

--- evoman_strategies/loop.sh
+++ evoman_strategies/loop.sh
@@ -2,7 +2,7 @@
 
 # run this script in your *activated* virtual environment
 
-for i in {2,6,7}; do
+for i in {1..8}; do
   'python3' set_config.py "FPS" "MCS" "$i" 100 40 1 -1
   'python3' compare_specialist.py
   'python3' plotting.py
