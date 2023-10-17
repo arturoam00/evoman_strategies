39cba4822ecc26f2f7219c95937c07c5f42ef1dd
Arturo Abril Martinez <arturoabrilm@gmail.com>
2023-10-17 17:09:49 +0200

    bug in environment fixed

--- evoman_strategies/environment_.py
+++ evoman_strategies/environment_.py
@@ -1,40 +1,42 @@
 import os
 from collections.abc import Iterable
+from copy import copy
 
 import numpy as np
 from evoman.environment import Environment
 
+default_params = {
+    "experiment_name": "specialist",
+    "multiplemode": "no",
+    "enemies": "2",
+    "loadplayer": "yes",
+    "loadplayer": "yes",
+    "loadenemy": "yes",
+    "level": 2,
+    "playermode": "ai",
+    "enemymode": "static",
+    "speed": "fastest",
+    "inputscoded": "no",
+    "randomini": "no",
+    "sound": "off",
+    "contacthurt": "player",
+    "logs": "off",
+    "savelogs": "yes",
+    "clockprec": "low",
+    "timeexpire": 3000,
+    "overturetime": 100,
+    "solutions": None,
+    "fullscreen": False,
+    "player_controller": None,
+    "enemy_controller": None,
+    "use_joystick": False,
+    "visuals": False,
+}
+
 
 class Environment_(Environment):
-    default_params = {
-        "experiment_name": "specialist",
-        "multiplemode": "no",
-        "enemies": "2",
-        "loadplayer": "yes",
-        "loadplayer": "yes",
-        "loadenemy": "yes",
-        "level": 2,
-        "playermode": "ai",
-        "enemymode": "static",
-        "speed": "fastest",
-        "inputscoded": "no",
-        "randomini": "no",
-        "sound": "off",
-        "contacthurt": "player",
-        "logs": "off",
-        "savelogs": "yes",
-        "clockprec": "low",
-        "timeexpire": 3000,
-        "overturetime": 100,
-        "solutions": None,
-        "fullscreen": False,
-        "player_controller": None,
-        "enemy_controller": None,
-        "use_joystick": False,
-        "visuals": False,
-    }
-
     def __init__(self, headless=True, **kwargs):
+        params = copy(default_params)
         if headless:
             os.environ["SDL_VIDEODRIVER"] = "dummy"
 
@@ -51,9 +53,9 @@ class Environment_(Environment):
             kwargs["multiplemode"] = "yes"
 
         kwargs["enemies"] = enemies
-        self.default_params.update(kwargs)
+        params.update(kwargs)
 
-        super().__init__(**self.default_params)
+        super().__init__(**params)
 
     def simulation(self, pcont):
         f, *_ = self.play(pcont=pcont)
